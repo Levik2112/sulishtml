078d948c869429a3d4e4d27b651b775014fe5e6b
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2022-11-04 10:35:24 +0100

    ad

---
+++ 2023/igen/python/fuggvenyek.py
@@ -0,0 +1,15 @@
+#pip install matplotlib --proxy http://tanulo01:Tanulo0118@172.16.0.5:8080
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0, 16])
+ypoints = np.array([0, 25])
+
+#plt.plot(xpoints, ypoints)
+#plt.plot([10,20],[32,10])
+
+#haz
+x=[0,50,50,0,0]
+y=[0,0,50,50,0]
+plt.plot(x, y)
+plt.show()
