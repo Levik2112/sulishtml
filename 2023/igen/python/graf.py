3703326dbab35a8cddbf6f3b3050ec8610196c8e
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2022-11-10 13:18:39 +0100

    rasd

---
+++ 2023/igen/python/graf.py
@@ -0,0 +1,50 @@
+#pip install matplotlib --proxy http://tanulo01:Tanulo0118@172.16.0.5:8080
+import matplotlib.pyplot as plt
+
+def fv(a,b,c):
+    xPont=[]
+    yPont=[]
+    
+    for x in range(-10,10):
+        xPont.append(x)
+        yPont.append(a*x**2 + b*x - c)
+
+    return[xPont,yPont]
+        
+#többedfoku egyenlet
+def fv2(*egyutthatok):
+    osszeg=0
+    for sorszam,i in enumerate(egyutthatok):
+        osszeg+=i * x**(len(egyutthatok)-1-sorszam)
+        print(i)
+x=[1,10]
+y=[10,1]
+#plt.plot(x,y)
+#plt.show()
+
+#y = 6x2 + 4x -16
+
+xPont=[]
+yPont=[]
+for x in range(-10,10):
+    xPont.append(x)
+    yPont.append(6*x**2 + 4*x - 16)
+
+plt.plot(xPont,yPont)
+
+
+#y = 16x2 - 3 + 16
+xPont=[]
+yPont=[]
+for x in range(-10,10):
+    xPont.append(x)
+    yPont.append(16*x**2 - 3 + 16)
+
+plt.plot(xPont,yPont)
+
+
+pontok=fv(1,1,1)
+plt.plot(pontok[0],pontok[1])
+
+#plt.show()
+fv2(1,2,3,4,5)
--- 2023/igen/python/masodfok.py
+++ 2023/igen/python/masodfok.py
@@ -1,5 +1,23 @@
 import math
 
+def egyenlet(a,b,c):
+    szoveg="0 = "
+    if a!=0:
+        szoveg+=str(a)+"x²"
+    if b>0:
+        szoveg+=" + "+str(b)+"x"
+    elif b<0:
+        szoveg+=" "+str(b)+"x"
+    if c>0:
+        szoveg+=" + "+str(c)+"x"
+    elif c<0:
+        szoveg+=" "+str(c)
+    return szoveg
+        
+
+        
+        
+
 a = int(input("a="))
 b = int(input("b="))
 c = int(input("c="))
@@ -17,10 +35,13 @@ elif diszkriminans==0:
     print("1 megoldás: {}".format(megoldas))
 else:
     x1=(-b+math.sqrt(diszkriminans)) / (2*a)
-    x1=(-b-math.sqrt(diszkriminans)) / (2*a)
+    x2=(-b-math.sqrt(diszkriminans)) / (2*a)
+    print("2 megoldás: x1:{},x2{}".format(x1,x2))
 
     
 
 
 #gyok=math.sqrt(1)
 #print(gyok)
+    
+print(egyenlet(a,b,c))
