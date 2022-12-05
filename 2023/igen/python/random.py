e8180138b0f63527d504b7743ec69fb54a456651
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2022-12-05 13:19:16 +0100

    asd

---
+++ 2023/igen/python/random.py
@@ -0,0 +1,14 @@
+import random
+import math
+l=[]
+for i in range(10):
+    szam=random.random()
+    l.append(math.floor(szam*90)+10)
+print(l)
+
+l=[]
+for i in range(10):
+    l.append(random.randint(10,99))
+    
+print(l)
+
