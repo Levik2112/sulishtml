8205caf00c846a6e84c3e4e0dccffdc9af234a6e
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2022-12-08 13:18:00 +0100

    asd

---
+++ 2023/igen/python/hazi12.08ű.py
@@ -0,0 +1,7 @@
+import random
+
+
+i=0
+while i!=1000:
+    print(random.randint(1900,1999),".",random.randint(1,12),".",random.randint(1,31))
+    i+=1
--- 2023/igen/python/random.py
+++ 2023/igen/python/random.py
@@ -4,11 +4,32 @@ l=[]
 for i in range(10):
     szam=random.random()
     l.append(math.floor(szam*90)+10)
-print(l)
+#print(l)
 
 l=[]
 for i in range(10):
     l.append(random.randint(10,99))
     
-print(l)
+#print(l)
 
+
+szamok=[]
+for i in range(100):
+    szamok.append((random.randint(-1000,1000)*3))
+print(szamok)
+
+
+#print(sum(szamok))  
+szamok5=[]
+for e in szamok:
+    if e%5==0:
+        szamok5.append(e)
+
+print()
+print()
+
+print(szamok5)
+
+
+szamok5=[e for e in szamok5 if e%5==0]
+print(szamok5)
