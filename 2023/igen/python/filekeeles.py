0945f6f951921f9cf20122b489b84bcfc6d127d5
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2023-01-12 13:18:42 +0100

    asd

--- 2023/igen/python/filekeeles.py
+++ 2023/igen/python/filekeeles.py
@@ -2,6 +2,23 @@
 
 f=open("proba.txt","w")
 f.write("helló")
+f.write("\n")
 f.write("világ")
 
+f.close()
+#file tartalma soronként 1
+f=open("proba.txt","r")
+
+szoveg=f.read()
+sorok=szoveg.split("\n")
+print(sorok)
+f.close()
+
+#file tartalma soronként 2
+
+f=open("proba.txt","r")
+sorok=[]
+for sor in f:
+     sorok.append(sor.replace("\n","").replace("\r",""))
+print(sorok)
 f.close()
