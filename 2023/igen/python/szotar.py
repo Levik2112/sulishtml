533d808328087bfc65df8bab6ea6d921db04e38f
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2023-01-18 14:04:34 +0100

    asd

---
+++ 2023/igen/python/szotar.py
@@ -0,0 +1,25 @@
+import random
+
+def szoBeker():
+    szo=input("Kérek egy szót: ")
+    if szo=="":
+        jelentes=""
+    else:
+        jelentes=input(szo+ " jelentése: ")
+        
+    return [szo,jelentes]
+
+szavak=[]
+def sokBeker():
+    szo=szoBeker()
+    while szo[0]!="":
+        szavak.append(szo)
+        szo=szoBeker()
+
+    return szavak
+    
+print(sokBeker())
+
+def filebaIr(lista):
+    f=open("szotar.txt","a)
+    
