be76611cfcf7a492d78a0c6bfcd825f15097089d
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2023-01-26 13:18:39 +0100

    asd

--- 2023/igen/python/szotar.py
+++ 2023/igen/python/szotar.py
@@ -54,11 +54,11 @@ def kerdez():
         #print("rossz",rossz)
         
 
-    print("-"*40)
+    print("-"*50)
     print("Mit jelent: "+ valasztott[0]+"?")
 
     rossz.append(valasztott)
-    print(rossz)
+    #print(rossz)
     #print(kerdesek)
     
     #válasz bekérés
@@ -69,25 +69,64 @@ def kerdez():
     for e in rossz:
         print(abc[i]+": "+e[1])
         i+=1
-    valasz=input("Válassz: ")
-    hol=abc.index(valasz)
-    print(hol)
 
+
+        
+    valasz=input("Válassz: ")
+    
+    hol=4
     while hol>=4:
         try:
-            valasz=input("Válassz újra: ")
-            hol=abc.index(valasz)
+            if valasz!="":
+                hol=abc.index(valasz)
+              
         except:
-            pass
+            valasz=input("Válassz újra: ")
+
+        else:   
+            if hol>=4:
+                valasz=input("Válassz újra: ")
+                  
+          
         
-        
+    #print(valasztott)
+    #print(rossz[hol])
+
+    #if valasztott[0]==rossz[hol][0]:
+     #   print("Helyes :)")
+    #else:
+    #    print("Rossz válasz!")
+
+    return valasztott[0]==rossz[hol][0]
+
+def menu():
+    beker=""
+    while beker!="0":
+        print("-"*50)
+        print("Szótár program\n")
+        print("1: Szavak bevitele")
+        print("2: Feleltetés")
+        print("0: Kilépés")
+        beker=input("Válassz: ")
+    
+    
+#feleltetés   
+#beolvas()
+#lil_A=[]
+#for i in range(10):
+  #  lil_A.append(kerdez())
+#print(lil_A)
+
+#print("Az eredmény: {:.0%}".format(lil_A.count(True)/len(lil_A)))
+
+
+
 
 
     
-    
-beolvas()
-kerdez()
-
+menu()
+#adatbekérés
+  
 #szavak=sokBeker()
 #filebaIr(szavak)
     
