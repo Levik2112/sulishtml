d8c57299c742bf994a9879ecd04e4d98be5e04ab
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2023-02-01 14:01:54 +0100

    asd

--- 2023/igen/erettsegi_feladatok/szamok.py
+++ 2023/igen/erettsegi_feladatok/szamok.py
@@ -16,3 +16,22 @@ f.close()
 
 print("2. feladat")
 print("Az adatfile-ban "+ str(len(kerdesek))+ " kérdés van.")
+
+
+matek=[]
+for e in kerdesek:
+    #print(e)
+    if e[3]=="matematika":
+        matek.append(e[2])
+
+print("Az adatfajlban "+str(len(matek)) + " matematika feladat van, 1 pontot er "+ str(matek.count(1)) +" feladat, 2 pontot er "+ str(matek.count(2)) +" feladat, 3 pontot er "+ str(matek.count(3)) +" feladat.")
+
+
+valaszokerteke=[]
+for e in kerdesek:
+    valaszokerteke.append(e[1])
+#print(valaszokerteke)
+
+valaszok=[e[1] for e in kerdesek]
+
+print("A válaszok számértéke {}-től {}-ig tart.".format(min(valaszok),max(valaszok)))
