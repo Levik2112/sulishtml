416761a5393aef42abea48a5ac7d65adaf905db0
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2023-02-07 11:27:58 +0100

    asd

--- 2023/igen/erettsegi_feladatok/tesztverseny/tesztverseny.py
+++ 2023/igen/erettsegi_feladatok/tesztverseny/tesztverseny.py
@@ -1,5 +1,4 @@
 
-
 f=open("valaszok.txt","r")
 
 adatok=f.read().split("\n")
@@ -12,6 +11,7 @@ f.close()
 
 helyes=adatok[0]
 adatok.remove(helyes)
+#print(helyes)
 
 valaszok=[]
 for e in adatok:
@@ -19,3 +19,32 @@ for e in adatok:
 
 #print(valaszok)
 
+print("2. feladat: A vetélkedőn "+str(len(valaszok))+" versenyző indult!")
+
+versenyzo=input("3. feladat: A versenyző azonosítója = ")
+
+#1.lehetőség
+versenyzoValasza=""
+for e in valaszok:
+    if e[0]==versenyzo:
+        print(e[1]+"\t a versenyző válasza.")
+        versenyzoValasza=e[1]
+#2.lehetőség
+#print("{} \t(a versenyző válasza)".format([e[1] for e in valaszok if e[0]==versenyzo[0]]))
+
+
+
+
+print("4.feladat")
+print(helyes+"\t a helyes megoldás.")
+print(versenyzoValasza)
+
+for sorszam,betu in enumerate(versenyzoValasza):
+    #print(betu,str(sorszam))
+
+    if betu==helyes[sorszam]:
+        print("+",end="")
+    else:
+        print(" ",end="")
+
+print("\t a versenyző helyes válaszai.")
