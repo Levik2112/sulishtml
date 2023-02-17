fc73551abd873dafea26b522c184b6e568947d0b
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2023-02-17 09:37:48 +0100

    asd

--- 2023/igen/agazativizsga_feladatok/feladat2.py
+++ 2023/igen/agazativizsga_feladatok/feladat2.py
@@ -1,3 +1,28 @@
 import haromszog
 
-print(haromszog.haromszog())
+def ujabb(darab=3):
+    haromszogek=[]
+    for i in range(darab):
+        haromszogek.append(haromszog.haromszog())
+
+    for e in haromszogek:
+        print("\ta={}, b={}, c={}".format(e[0],e[1],e[2]))
+        #3,lista,3számmal l=[[1,2,3],[4,5,6],[7,8,9]]
+
+    return haromszogek
+
+
+
+def haromszogE(haromszogek):
+    for e in haromszogek:
+        if sum(e)-max(e)>max(e):
+            print("Lehet háromszög")
+        else:
+            print("Nem lehet háromszög.")
+                            
+                            
+
+#print(haromszog.haromszog())
+    
+h=ujabb(3)
+haromszogE(h)
