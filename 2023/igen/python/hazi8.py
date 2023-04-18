aaf7476e08077f37ee9a7f1051e96d16a4947f4f
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2023-04-18 12:30:52 +0200

    asd

--- 2023/igen/python/hazi8.py
+++ 2023/igen/python/hazi8.py
@@ -3,4 +3,18 @@ class auto:
     #indulaskor mondja azt hogy brrr,hangjelzes(kurt,duda=tütü),irányjlező=katkatkat,
     #leszarmazott amelyik bmw,indul=vrumm,iranyjelzo=néma,
     #mégegy class mercedes néven kürt=vmi mást,
-    #3. auto audi miden tulajdonság ami eddig,check engine de ha megnyomjak a kurtot akkor bekapcsol 
+    #3. auto audi miden tulajdonság ami eddig,check engine de ha megnyomjak a kurtot akkor bekapcsol
+
+    def __init__(self,szin,ajtokszama,marka,tipus):
+        self.szin=szin
+        self.ajtokszama=ajtokszama
+        self.marka=marka
+        self.tipus=tipus
+
+    def inditas(self):
+        print("Brrr")
+    def duda(self):
+        print("Tütü")
+teszt=auto("piros",3,"bmw","e34")
+print(teszt.inditas())
+print(teszt.duda())
