6bf97e267ca52938aff24c4361096ae7a28d3efc
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2023-04-18 12:23:17 +0200

    asd

---
+++ 2023/igen/python/hazi8.py
@@ -0,0 +1,6 @@
+class auto:
+    szin,ajtokszama,marka,tipus=["",0,"",""]
+    #indulaskor mondja azt hogy brrr,hangjelzes(kurt,duda=tütü),irányjlező=katkatkat,
+    #leszarmazott amelyik bmw,indul=vrumm,iranyjelzo=néma,
+    #mégegy class mercedes néven kürt=vmi mást,
+    #3. auto audi miden tulajdonság ami eddig,check engine de ha megnyomjak a kurtot akkor bekapcsol 
---
+++ 2023/igen/python/oop gyakorlat.py
@@ -0,0 +1,30 @@
+class muvelet:
+    x=0
+    y=0
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+
+    def osszeadas(self):
+        return self.x+self.y
+    def kivonas(self):
+        return self.x-self.y
+
+    def osztas(self):
+        return self.x/self.y
+
+    def szorzas(self):
+        return self.x*self.y
+    
+    
+
+
+
+
+if __name__=='__main__':
+    #tesztelés
+    q=muvelet(1,2)
+    print(q.osszeadas())
+    print(q.kivonas())
+    print(q.osztas())
+    print(q.szorzas())
