b750b3f405046162781465cc5ebf3b6078ea74ce
Dobi Levente Domonkos <dobil21@ADY-NAGYATAD.SULINET.HU>
2023-04-19 13:18:56 +0200

    asd

--- 2023/igen/projekt/monogram_mukodo.py
+++ 2023/igen/projekt/monogram_mukodo.py
@@ -2,24 +2,8 @@
 from tkinter import *
 import math
 
-class forgato:
-    canvas=0
-    vonalak=[]
-    def __init__(self,canvas,vonalak):
-        self.canvas=canvas
-        self.vonalak=vonalak
-    def rajzol(self):
-        canvas.delete("all")
-        szog+=.5
-        print(szog)
-        for betu in self.vonalak:
-            betu=eltol(betu,-kozep[0],-kozep[1])
-            betu=nagyit(betu,0.5)
-            betu=forgat(betu,szog)
-            betu=eltol(betu,kozep[0],kozep[1])
-            
-            self.canvas.create_line(betu,fill="green",width=1)
-          
+
+
 def eltol(pontok,x,y):
     vissza=[]
     for i, pont in enumerate(pontok):
@@ -80,7 +64,6 @@ pontok0 = [10,10,30,10,30,10,90,60,90,60,150,10,150,10,170,10,150,170,170,170,10
 
 M = [10,10,30,10,90,60,150,10,170,10,170,170,150,170,150,30,90,80,30,30,30,170,10,170,10,10]
 nev=[]
-elso=forgato(canvas,M)
 for i in range(5):
     nev.append(eltol(M,100*i,10))
 
@@ -99,10 +82,18 @@ kozep[0]/=db
 kozep[1]/=db
 szog=0
 while True:
-    win.update_idletasks()
-    win.update()
-
-    
+    canvas.delete("all")
+    szog+=.5
+    print(szog)
+    for betu in nev:
+        betu=eltol(betu,-kozep[0],-kozep[1])
+        betu=nagyit(betu,0.5)
+        betu=forgat(betu,szog)
+        betu=eltol(betu,kozep[0],kozep[1])
+        
+        canvas.create_line(betu,fill="green",width=1)
+        win.update_idletasks()
+        win.update()
     
 
 
